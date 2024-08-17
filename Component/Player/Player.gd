e56266934957e57199c0c788059b8edd17bf2e22
Mohammed Qureshi <mohammedqureshi2998@gmail.com>
2024-08-17 17:32:34 +0100

    Update mouse input to not run every time

--- Component/Player/Player.gd
+++ Component/Player/Player.gd
@@ -23,7 +23,8 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	print(player_id)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	if multiplayer.get_unique_id() == player_id:
 		camera_3d.make_current()
 	else:
@@ -31,16 +32,12 @@ func _ready():
 
 
 func _apply_movement_from_input(delta):
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	#if Input.is_action_just_pressed("Escape"):
-		#isCaptured = !isCaptured
 
 	var input_dir = %InputSynchronizer.input_direction
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -57,12 +54,11 @@ func _input(event):
 	if Input.is_action_just_pressed("Escape"):
 		print("Hello")
 		isCaptured = !isCaptured
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if isCaptured else Input.MOUSE_MODE_VISIBLE)
 	
 	if Input.is_action_just_pressed("ForceQuit"):
 		get_tree().quit()
 		
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if isCaptured else Input.MOUSE_MODE_VISIBLE)
-	
 	if event is InputEventMouseMotion and isCaptured:
 		$Head.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Head.rotation.x = clamp($Head.rotation.x, deg_to_rad(-45), deg_to_rad(45))
